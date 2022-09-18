689392e406935fa295852ed096c3ec021b396eae
mirzaramzanshabbir <113830068+mirzaramzanshabbir@users.noreply.github.com>
2022-09-18 03:34:23 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+function saygratitude(students) {
+    for (var i = 0; i < studentlist.length; i++) {
+        if (studentlist[i] == "naveed") {
+            console.log('hi,${studentlist[i]} welcome to universit, you are top scorer in entery TextDecoderStream,cingrats');
+        }
+        else {
+            console.log('hi, ${studentlist[i]} welcome to university');
+        }
+    }
+}
+var studentlist = ["ali", "umar", "haider", "waseem", "qamar", "saad", "nisar", "faheem", "ansar", "anas"];
---
+++ app.ts
@@ -0,0 +1,11 @@
+function saygratitude(students?:string){
+    for(var i=0; i < studentlist.length; i++){
+        if(studentlist[i] == "naveed"){
+            console.log('hi,${studentlist[i]} welcome to universit, you are top scorer in entery TextDecoderStream,cingrats')
+        } else{
+            console.log('hi, ${studentlist[i]} welcome to university');
+        }   
+    }
+}
+
+var studentlist:string[] = ["ali", "umar", "haider","waseem","qamar","saad","nisar","faheem","ansar","anas"];
